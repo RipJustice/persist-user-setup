4351ba8f17d7549699b6b6102629564ec2f7e0ee
smullen <mrhoipoi@gmail.com>
2021-03-17 16:01:39 -0400

    Added persistance to local state, refactored state logic

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -3,7 +3,6 @@ import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
 import './form.scss';
-import { waitForDomChange } from '@testing-library/dom';
 
 const FormTitle = () => {
     return <h1>Create User</h1>;
@@ -17,24 +16,32 @@ const Submit = () => {
     );
 }
 
-function UserForm() {
-    const [uName, setUName] = useState<string>();
-    const [pWord, setPWord] = useState<string>();
-    const [pWordCompare, setPWordCompare] = useState<string>();
-    const [invalid, setInvalid] = useState<string>('');
+interface userDefined {
+    uName: string;
+    pWord: string;
+    pWordCompare: string;  
+}
 
+function UserForm() {
+    // this could be session storage or redux as well, I simply interpreted persist as meaning persist to local
+    const [user, setUser] = useState<userDefined>(
+        (localStorage.getItem('user_store')) ? JSON.parse(localStorage.getItem('user_store') || '') :
+        {uName: '', pWord: '', pWordCompare: ''}       
+    );
+    const [invalid, setInvalid] = useState<string>('');
+    
     useEffect(() => {
-        
-    }, []);
+        localStorage.setItem('user_store', JSON.stringify(user));
+    }, [user]);
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        if (uName && pWord && pWordCompare) {
-            if (pWord === pWordCompare) {
+        if (user.uName.trim() && user.pWord.trim() && user.pWordCompare.trim()) {
+            if (user.pWord === user.pWordCompare) {
                 alert('Success!');
-                setInvalid('');
+                setInvalid('');                            
             } else {
-                alert('Your password does not match verification');  
-                setInvalid('p-invalid');
+                alert('Your password does not match verification'); 
+                setInvalid('p-invalid');                             
             }
         } else {
             alert('Please fill out all fields');
@@ -47,19 +54,19 @@ function UserForm() {
             <FormTitle />
             <div className="p-field p-grid p-dir-col"> 
                 <label htmlFor="uname" className="p-d-block">First Name</label>
-                <InputText id="uname" name='userName' value={uName}  onChange={(e: ChangeEvent<HTMLInputElement>) => setUName(e.target.value)} onBlur={(e: ChangeEvent<HTMLInputElement>) => setUName(e.target.value)}/>
+                <InputText id="uname" name='userName' value={user.uName}  onChange={(e: ChangeEvent<HTMLInputElement>) => setUser((prevState: userDefined) => ({...prevState, uName: e.target.value}))} onBlur={(e: ChangeEvent<HTMLInputElement>) => setUser((prevState: userDefined) => ({...prevState, uName: e.target.value}))}/>
             </div>
             <div className="p-field p-grid p-dir-col"> 
                 <label htmlFor="pword" className="p-d-block">Password</label>
-                <Password id="pword" className={invalid} name='password' value={pWord} onChange={(e: ChangeEvent<HTMLInputElement>) => setPWord(e.target.value)} onBlur={(e: ChangeEvent<HTMLInputElement>) => setPWord(e.target.value)} toggleMask/>
+                <Password id="pword" className={invalid} name='password' value={user.pWord} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser((prevState: userDefined) => ({...prevState, pWord: e.target.value}))} onBlur={(e: ChangeEvent<HTMLInputElement>) => setUser((prevState: userDefined) => ({...prevState, pWord: e.target.value}))} toggleMask/>
             </div>
             <div className="p-field p-grid p-dir-col"> 
-                <label htmlFor="pWordCompare" className="p-d-block">Verify Password</label>
-                <Password id="pWordCompare" className={invalid} name='password' value={pWordCompare} onChange={(e: ChangeEvent<HTMLInputElement>) => setPWordCompare(e.target.value)} onBlur={(e: ChangeEvent<HTMLInputElement>) => setPWordCompare(e.target.value)} toggleMask/>
+                <label htmlFor="pWordCompare" className="p-d-block">Confirm Password</label>
+                <Password id="pWordCompare" className={invalid} name='password' value={user.pWordCompare} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser((prevState: userDefined) => ({...prevState, pWordCompare: e.target.value}))} onBlur={(e: ChangeEvent<HTMLInputElement>) => setUser((prevState: userDefined) => ({...prevState, pWordCompare: e.target.value}))} toggleMask/>
             </div>
             <div className="p-field p-text-right p-grid p-dir-col">                     
                 <Submit />
-            </div>                
+            </div> 
         </form> 
     );
 }
